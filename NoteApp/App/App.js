10ff3eafa00cd492aa26ad4466167a0f65f7bd9e
Brooke Lewis <belv2c@gmail.com>
2018-06-12 20:35:51 -0500

    setup angular routing and added text area for notes

---
+++ NoteApp/App/App.js
@@ -0,0 +1,11 @@
+﻿var app = angular.module("NoteApp", ["ngRoute", "ui.bootstrap"]);
+
+app.config(["$routeProvider", function ($routeProvider) {
+    $routeProvider.when("/",
+        {
+            templateUrl: "/app/partials/home.html",
+            controller: "HomeController"
+        })
+
+        .otherwise('/');
+}]);
