614f3fe2025e0a42a4f6858415f219273315cace
Brooke Lewis <belv2c@gmail.com>
2018-06-16 13:01:22 -0500

    frontend is returning data

--- NoteApp/App/Controllers/HomeController.js
+++ NoteApp/App/Controllers/HomeController.js
@@ -1,5 +1,22 @@
-﻿app.controller("HomeController", ["$scope", "$http",
-    function ($scope, $http) {
+﻿app.controller("HomeController", ["$scope", "$http", "HomeService",
+    function ($scope, $http, HomeService) {
         $scope.message = "this is a test";
+
+        var getAllNotes = function () {
+            HomeService.getAllNotes().then(function (results) {
+                $scope.notes = results;
+            }).catch(function (error) {
+                console.log("error in getAllNotes", error);
+            });
+        }();
+
+        const addNewNote = function () {
+            HomeService.addNewNote($scope.note).then(function (results) {
+                console.log(results);
+            }).catch(function (err) {
+                console.log("error in addDepartment in controller", err);
+            });
+        };
+
     }
 ])
